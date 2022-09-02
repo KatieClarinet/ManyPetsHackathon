6e2fc03201749ac3fa7d58d09340d9a39f191467
nafisoaden97 <nafisoaden97@gmail.com>
2022-09-02 12:28:44 +0100

    working on logic for calculation

--- routes/routes.js
+++ routes/routes.js
@@ -2,21 +2,24 @@ import express from "express";
 
 const router = express.Router();
 
-async function newQuote({reqData}){
-    console.log(reqData)
+async function newQuote({ reqData }) {
+  if (reqData.pet.age === 5) {
+    return reqData.baseprice * ((reqData.pet.age * 5) / 100);
+  }
+
+  console.log(reqData.baseprice);
 }
 
-
-router.get("/", async function(req, res){
-      res.json({
-          success : true,
-          message : "Here are your tasks" });
+router.get("/", async function (req, res) {
+  res.json({
+    success: true,
+    message: "Here are your tasks",
+  });
 });
 
-router.post("/quote", async function(req, res){
-    const reqData = req.body;
-    const quote = await newQuote({reqData});
-    res.json({ success: true,
-               payload: reqData});
-})
+router.post("/quote", async function (req, res) {
+  const reqData = req.body;
+  const quote = await newQuote({ reqData });
+  res.json({ success: true, payload: quote });
+});
 export default router;
