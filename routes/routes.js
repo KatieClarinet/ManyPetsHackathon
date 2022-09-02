0f1ae496dc25ac81bc501a74a42685f2dcd61d72
halil-coskun <halilcoskun14@gmail.com>
2022-09-02 12:04:21 +0100

    post

--- routes/routes.js
+++ routes/routes.js
@@ -2,16 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
+async function newQuote({reqData}){
+    console.log(reqData)
+}
+
+
 router.get("/", async function(req, res){
       res.json({
           success : true,
           message : "Here are your tasks" });
 });
 
-router.post("/user_table", async function(req, res){
+router.post("/quote", async function(req, res){
     const reqData = req.body;
-    const newTask = await postToTasks({reqData});
+    const quote = await newQuote({reqData});
     res.json({ success: true,
-               payload: newTask});
+               payload: reqData});
 })
 export default router;
