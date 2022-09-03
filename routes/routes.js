605817c470915695ece1e3cd7a1863cf493bfb35
Katie Walters <katie_walters@live.co.uk>
2022-09-03 17:33:25 +0100

    experimenting with getting multipets discount to work

--- routes/routes.js
+++ routes/routes.js
@@ -1,109 +1,116 @@
 import express from "express";
-
 const router = express.Router();
 
+//change index to be number of pets
+var index = 0;
 router.get("/", async function (req, res) {
-  res.json({
-    success: true,
-    message: "Here are your tasks",
-  });
+    res.json({
+        success: true,
+        message: "Here are your tasks",
+    });
 });
 
 router.post("/quote", async function (req, res) {
-  let reqData = req.body;
-  let quote = await ageQuote(reqData);
-  console.log(`quote ${quote}`)
-  let breed = await breedCalculator(reqData,quote);
-  console.log(`breed ${breed}`)
-  let postcode = await postcodeCalculator(reqData, breed);
-  console.log(`postcode ${postcode}`)
-  res.json({ success: true, AgeBasedQuote: quote, IncBreedDiscount: breed, IncPostcodeCost: postcode });
+    let reqData = req.body;
+    //pet1
+    let quote = await ageQuote(reqData, index);
+    let breed = await breedCalculator(reqData, quote, index);
+    let postcode = await postcodeCalculator(reqData, breed);
+    let multiPets = await checkMultiPets(reqData, index);
+    //pet2
+    let quote1 = await ageQuote(reqData, index);
+    let breed1 = await breedCalculator(reqData, quote, index);
+    //pet3
+    let quote2 = await ageQuote(reqData, index);
+    let breed2 = await breedCalculator(reqData, quote, index);
+    console.log(`multipets ${multiPets}`);
+    res.json({
+        success: true,
+        Pet1: {
+            AgeBasedQuote: quote,
+            IncBreedDiscount: breed,
+            IncPostcodeCost: postcode,
+        },
+        Pet2: { AgeBasedQuote: quote1, IncBreedDiscount: breed1 },
+        Pet3: { AgeBasedQuote: quote2, IncBreedDiscount: breed2 },
+    });
 });
 
-async function ageQuote(reqData) {
-  if (reqData.pet.age === 1) {
-    return reqData.baseprice * 1.05;
-  }
-  if (reqData.pet.age === 2) {
-    return reqData.baseprice * 1.1;
-  }
-  if (reqData.pet.age === 3) {
-    return reqData.baseprice * 1.15;
-  }
-  if (reqData.pet.age === 4) {
-    return reqData.baseprice * 1.2;
-  }
-  if (reqData.pet.age === 5) {
-    return reqData.baseprice * 1.25;
-  }
-  if (reqData.pet.age === 6) {
-    return reqData.baseprice * 1.35;
-  }
-  if (reqData.pet.age === 7) {
-    return reqData.baseprice * 1.45;
-  }
-  if (reqData.pet.age === 8) {
-    return reqData.baseprice * 1.55;
-  }
-  if (reqData.pet.age === 9) {
-    return reqData.baseprice * 1.65;
-  } else {
-    return reqData.baseprice * 1.75;
-  }
-}
- 
-  async function breedCalculator(reqData,quote) {
-    
-    //SEND FETCH REQ here to check breed 
-    if (
-      reqData.pet.breed.toLowerCase() === "husky" ||
-      reqData.pet.breed.toLowerCase() === "labrador" ||
-      reqData.pet.breed.toLowerCase() === "german shepherd" 
-      ) {
-        let breedDiscount = quote * 0.9;
-        // console.log(`breedDiscount ${breedDiscount}`)
-        return breedDiscount
-      } else {
-        // console.log(`breed line 65 ${reqData.pet.breed}`)
-        return quote;
-      }
-  }
-
-  async function postcodeCalculator(reqData, breed) {
-    if (
-      reqData.postcode.toLowerCase().includes("bs4") ||
-      reqData.postcode.toLowerCase().includes("sw3") ||
-      reqData.postcode.toLowerCase().includes("m12") 
-    ) {
-      let postcodeIncrease = breed * 1.15
-      // console.log(`postcodeIncrease ${postcodeIncrease}`)
-      return postcodeIncrease
-    } else {
-      // console.log(`postcode line 83${reqData.postcode}`)
-      return breed;
+async function ageQuote(reqData, index) {
+    console.log(`ageQuote line 40 index: ${index}`);
+    console.log(`ageQuote line 41 pet age: ${reqData.pets[index].age}`)
+    if (reqData.pets[index].age <= 1) {
+        return reqData.baseprice * 1.05;
     }
-  }
-// }
-// let increase5 = reqData.pet.age * 0.05;
-// let less5AgeQuote = reqData.baseprice + (reqData.baseprice * increase5);
-//     return less5AgeQuote
-//   }
-//   else if (reqData.pet.age > 5 && reqData.pet.age <= 10) {
-//     let increase10 = reqData.pet.age * 0.1;
-//     let less10AgeQuote = reqData.baseprice + (reqData.baseprice * increase10);
-//         return less10AgeQuote
+    if (reqData.pets[index].age === 2) {
+        return reqData.baseprice * 1.1;
+    }
+    if (reqData.pets[index].age === 3) {
+        return reqData.baseprice * 1.15;
+    }
+    if (reqData.pets[index].age === 4) {
+        return reqData.baseprice * 1.2;
+    }
+    if (reqData.pets[index].age === 5) {
+        return reqData.baseprice * 1.25;
+    }
+    if (reqData.pets[index].age === 6) {
+        return reqData.baseprice * 1.35;
+    }
+    if (reqData.pets[index].age === 7) {
+        return reqData.baseprice * 1.45;
+    }
+    if (reqData.pets[index].age === 8) {
+        return reqData.baseprice * 1.55;
+    }
+    if (reqData.pets[index].age === 9) {
+        return reqData.baseprice * 1.65;
+    } else {
+        return reqData.baseprice * 1.75;
+    }
+}
+//calculate 10% discount for certain breeds
+async function breedCalculator(reqData, quote) {
+  console.log(`breedCalculator line 74 index: ${index}`);
+  console.log(`breedCalculator line 74 pet breed: ${reqData.pets[index].breed}`)
+    if (
+        reqData.pets[index].breed.toLowerCase() === "husky" ||
+        reqData.pets[index].breed.toLowerCase() === "labrador" ||
+        reqData.pets[index].breed.toLowerCase() === "german shepherd"
+    ) {
+        return quote * 0.9;
+    } else {
+        return quote;
+    }
+}
+//calculate 15% increase for certain areas
+async function postcodeCalculator(reqData, breed) {
+    if (
+        reqData.postcode.toLowerCase().includes("bs4") ||
+        reqData.postcode.toLowerCase().includes("sw3") ||
+        reqData.postcode.toLowerCase().includes("m12")
+    ) {
+        return breed * 1.15;
+    } else {
+        return breed;
+    }
+}
 
-// else {
-//   let oldPets = reqData.baseprice * 1.75
-//   return oldPets
-// }
-
-//  if (reqData.postcode.slice(0, 4) === BS4 ||reqData.postcode.slice(0, 4) === SW3 ||reqData.postcode.slice(0, 4) === M12) {
-//    return
-//   res.json({ success: true, payload: "expensive postcode" });
-//  }
-
-// 10% discount for Husky, Labrador, German Shepherd
-// 15% increase for postcodes
+// console.log(`index line 80 ${index}`);
+//need to check here if there are more pets in same object
+//and if so, to increase the index then start the loop again
+//but if not...stop
+//if reqData.pets[index++] != null then loop back over?
+//check if the reqData array contains another index?
+async function checkMultiPets(reqData, index) {
+    console.log(`checkMultiPets line 106 Pets array length: ${reqData.pets.length}`);
+    if (reqData.pets.length > index) {
+        index = index + 1;
+        console.log(`index line 109 ${index}`);
+        ageQuote(reqData, index);
+    } else {
+        console.log(`number of pets = ${index} -1`);
+    }
+}
 
 export default router;
